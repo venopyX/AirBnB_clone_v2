6902b45510813051b752bc5724a08e359533c064
venopyX <gladsonchala@gmail.com>
2025-01-05 21:58:50 +0300

    db storage update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" new class for sqlAlchemy """
+"""Module for database storage using SQLAlchemy"""
 from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
 from models.state import State
@@ -14,32 +14,42 @@ from models.amenity import Amenity
 
 
 class DBStorage:
-    """ create tables in environmental"""
+    """Database storage class for managing database operations"""
     __engine = None
     __session = None
 
     def __init__(self):
+        """Initialize database storage with environment variables.
+
+        Creates engine connection to MySQL database using environment
+        variables for credentials. Drops all tables if in test environment.
+        """
         user = getenv("HBNB_MYSQL_USER")
         passwd = getenv("HBNB_MYSQL_PWD")
         db = getenv("HBNB_MYSQL_DB")
         host = getenv("HBNB_MYSQL_HOST")
         env = getenv("HBNB_ENV")
 
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(user, passwd, host, db),
-                                      pool_pre_ping=True)
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(user, passwd, host, db),
+            pool_pre_ping=True
+        )
 
         if env == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of __object
+        """Query all objects or objects of a specific class.
+
+        Args:
+            cls (class, optional): Class to query objects for. Defaults to None.
+
+        Returns:
+            dict: Dictionary of objects keyed by <class name>.<id>
         """
         dic = {}
         if cls:
-            if type(cls) is str:
+            if isinstance(cls, str):
                 cls = eval(cls)
             query = self.__session.query(cls)
             for elem in query:
@@ -52,33 +62,38 @@ class DBStorage:
                 for elem in query:
                     key = "{}.{}".format(type(elem).__name__, elem.id)
                     dic[key] = elem
-        return (dic)
+        return dic
 
     def new(self, obj):
-        """add a new element in the table
+        """Add a new object to the current database session.
+
+        Args:
+            obj: Object to add to session
         """
-        self.__session.add(obj)
+        if obj:
+            self.__session.add(obj)
 
     def save(self):
-        """save changes
-        """
+        """Commit all changes of the current database session."""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete an element in the table
+        """Delete object from current database session.
+
+        Args:
+            obj (optional): Object to delete. Defaults to None.
         """
         if obj:
-            self.session.delete(obj)
+            self.__session.delete(obj)
+            self.save()
 
     def reload(self):
-        """configuration
-        """
+        """Create all tables in database and create session."""
         Base.metadata.create_all(self.__engine)
-        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sec)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
-        """ calls remove()
-        """
+        """Close the current session."""
         self.__session.close()
